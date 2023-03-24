e0f8baed8c5d89a15acab3e7dcf3d33d66f80f62
ViralVerity <verity.hill@ed.ac.uk>
2023-03-24 13:41:01 -0400

    add colour commands from civet

--- denv_pipeline/utils/misc.py
+++ denv_pipeline/utils/misc.py
@@ -30,3 +30,22 @@ def check_input_files(config):
     #if not symlink, does it exist and is the structure correct - ie sample_name folder with 2xfastq files in it with R1 and R2 on them
 
     return
+
+END_FORMATTING = '\033[0m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[93m'
+CYAN = '\u001b[36m'
+DIM = '\033[2m'
+
+def red(text):
+    return RED + text + END_FORMATTING
+
+def cyan(text):
+    return CYAN + text + END_FORMATTING
+
+def green(text):
+    return GREEN + text + END_FORMATTING
+
+def yellow(text):
+    return YELLOW + text + END_FORMATTING
