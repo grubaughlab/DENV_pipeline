82df2df9f4eb35009979c63720fc18aa293039d8
ViralVerity <verity.hill@ed.ac.uk>
2023-05-02 11:43:13 -0400

    remove old call to check paths

--- denv_pipeline/command.py
+++ denv_pipeline/command.py
@@ -65,7 +65,6 @@ def main(sysargs = sys.argv[1:]):
     for arg_name, arg_value in vars(args).items():
         config = misc.add_arg_to_config(arg_name, arg_value, config)
     
-    error_checks.check_paths(config)
     config["outdir"] = config["outdir"].rstrip("/")
     config["tempdir"] = os.path.join(config["outdir"], config["tempdir"])
     config = set_up_scripts.set_up_reference_directory(config)
