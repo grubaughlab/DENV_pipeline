ea83c710de46c4cdf758570be31452c166209266
ViralVerity <verity.hill@ed.ac.uk>
2023-06-15 16:06:33 -0400

    start on full run unit tests

--- tests/scripts/unit_tests_full_run.py
+++ tests/scripts/unit_tests_full_run.py
@@ -7,6 +7,8 @@ from Bio import SeqIO
 
 sample_names = ["denv1_test", "denv2_test", "denv3_test", "denv4_test", "low_coverage"]
 correct_serotype = {"denv1_test": ["DENV1"], "denv2_test":["DENV2"],  "denv3_test":["DENV3"],  "denv4_test":["DENV4", "DENV4_sylvatic"] }
+correct_sero_to_seq = {'DENV1': 'denv1_test', 'DENV2': 'denv2_test', 'DENV3': 'denv3_test', 'DENV4': 'denv4_test', 'DENV4_sylvatic': 'denv4_test'}
+serotypes = ["DENV1", "DENV2", "DENV3", "DENV4", "DENV4_sylvatic"]
 
 def check_alignments_present(outdir):
 
@@ -25,22 +27,32 @@ def check_alignments_present(outdir):
 
 def check_alignments_correct(outdir):
 
+    alignment_dir = os.path.join(outdir, "alignments")
 
-
-
+    for sero in serotypes:
+        correct_sequence = f'{correct_sero_to_seq[sero]}.{sero}'
+        for seq in SeqIO.parse(os.path.join(alignment_dir, f"{sero}.trim.aln"), 'fasta'):
+            assert seq.id == correct_sequence
+        for seq in SeqIO.parse(os.path.join(alignment_dir, f"{sero}.untrim.aln"), 'fasta'):
+            assert seq.id == correct_sequence
 
     return
 
 
 def check_bams_present(outdir):
 
+    for sample in sample_names:
+        if sample in correct_serotype:
+            for sero in correct_serotype[sample]:
 
+                bam_file = os.path.join(outdir, "results", "bam_files", f"{sample}.{sero}.sort.bam")
+                bai_file = os.path.join(outdir, "results", "bam_files", f"{sample}.{sero}.sort.bam.bai")
 
-    return
+                assert os.path.exists(bam_file)
+                assert os.path.exists(bai_file)
 
-
-def check_bams_correct(outdir):
-
+                assert os.stat(bam_file).st_size > 0
+                assert os.stat(bai_file).st_size > 0
 
     return
 
@@ -98,7 +110,6 @@ def run_tests():
     check_alignments_correct(outdir)
 
     check_bams_present(outdir)
-    check_bams_correct(outdir)
 
     check_consensus_present(outdir)
     check_consensus_correct(outdir)
